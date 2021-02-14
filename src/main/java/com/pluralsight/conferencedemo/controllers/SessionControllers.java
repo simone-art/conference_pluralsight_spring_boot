d2fb2d67294c2db48c7f2754001c4691c45e0e94
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-14 11:34:49 -0300

    "Criados métodos delete e Update na classe Session "

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionControllers.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionControllers.java
@@ -2,6 +2,7 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repository.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,8 +33,28 @@ public class SessionControllers {
 
     @PostMapping
     //saveAndFlush é um método do JPA e tem a mesma funcão do save pertencente ao Crud
+    //Também ela guarda e vacia a database
     public Session create(@RequestBody final Session session){
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    // Método para deletar pelo Id
+    public void delete(@PathVariable Long id){
+        //É preciso checar a existência desse id antes de deletar
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.PUT)
+    // Método para atualizar buscando pelo id
+    // existingSession é uma variável  que valida antes a existência daquele id
+    // A função do BeanUtils é pegar os dados existente daquele id
+    //copyProperties = copia os dados da sessão que queremos atualizar
+    //SaveAndFlush = salve, vacia e atualiza novamente o dado
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 
 
