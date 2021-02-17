103c158da9f0d82f51e89d0b87ebb31f346a6091
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-17 15:22:34 -0300

    "Comentado código pra visualizar Api no Heroku"

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -24,49 +24,49 @@ public class PersistenceConfiguration {
     //Unable to create requested service [org.hibernate.engine.jdbc.env.spi.JdbcEnvironment] por isso tireo o Bean
     //Método que retorna dados de origem
 
-
-    @Bean
-    public DataSource datasource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:mysql://localhost:3306/conference_app?createDatabaseIfNotExist=true&serverTimezone=UTC");
-        builder.username("root");
-        builder.password("simone23");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
-    }
-
-    //Tem a responsabilidade de criar o hibernate session factory
-    @Bean(name="entityManagerFactory")
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(datasource());
-        //Scaneia o pacote das entidades que estão na pasta models
-        sessionFactory.setPackagesToScan(new String[]{"com.pluralsight.conferencedemo.models"});
-        return sessionFactory;
-    }
-
-    @Bean
-    @Autowired
-    //Deletado o hibernate porque está dando erro e incluido o JpaTransactionManager
-    //Com o JPA rodou com sucesso e consegui ver a api na tela
-    public JpaTransactionManager transactionManager(final SessionFactory sessionFactory){
-        final JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(sessionFactory);
-        return txManager;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(datasource());
-        entityManager.setPackagesToScan(new String[] { "com.pluralsight.conferencedemo.models\"" });
-
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        entityManager.setJpaVendorAdapter(vendorAdapter);
-        //entityManager.setJpaProperties(additionalProperties());
-
-        return entityManager;
-    }
-
+//
+//    @Bean
+//    public DataSource datasource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:mysql://localhost:3306/conference_app?createDatabaseIfNotExist=true&serverTimezone=UTC");
+//        builder.username("root");
+//        builder.password("simone23");
+//        System.out.println("My custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
+//
+//    //Tem a responsabilidade de criar o hibernate session factory
+//    @Bean(name="entityManagerFactory")
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(datasource());
+//        //Scaneia o pacote das entidades que estão na pasta models
+//        sessionFactory.setPackagesToScan(new String[]{"com.pluralsight.conferencedemo.models"});
+//        return sessionFactory;
+//    }
+//
+//    @Bean
+//    @Autowired
+//    //Deletado o hibernate porque está dando erro e incluido o JpaTransactionManager
+//    //Com o JPA rodou com sucesso e consegui ver a api na tela
+//    public JpaTransactionManager transactionManager(final SessionFactory sessionFactory){
+//        final JpaTransactionManager txManager = new JpaTransactionManager();
+//        txManager.setEntityManagerFactory(sessionFactory);
+//        return txManager;
+//    }
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+//        entityManager.setDataSource(datasource());
+//        entityManager.setPackagesToScan(new String[] { "com.pluralsight.conferencedemo.models\"" });
+//
+//        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        entityManager.setJpaVendorAdapter(vendorAdapter);
+//        //entityManager.setJpaProperties(additionalProperties());
+//
+//        return entityManager;
+//    }
+//
 
 }
